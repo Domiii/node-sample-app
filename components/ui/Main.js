cbeb19022ed3b9164b1fe46ba24812c4397ced82
chengchieh <chengchieh800106@hotmail.com>
2015-01-21 21:34:17 +0800

    0121_create_view

--- components/ui/Main.js
+++ components/ui/Main.js
@@ -158,7 +158,7 @@ module.exports = NoGapDef.component({
         	 */
             {
                 pageComponents: [
-                    'HomePage'
+                    'main_layout'
                 ],
 
                 /**
